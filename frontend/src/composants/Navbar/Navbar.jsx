0e687cdb4894f0b14f42fec9a80a5c2bc7196e08
Asle Sayed <asle@Asles-MacBook-Pro.local>
2024-05-17 00:44:01 +0200

    navbar in progress

--- frontend/src/composants/Navbar/Navbar.jsx
+++ frontend/src/composants/Navbar/Navbar.jsx
@@ -1,15 +1,36 @@
+
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
   return (
-    <div className="whole-navbar">
-      <div className="top-navbar">
-        <div className="logo">CINEREVIEW</div>
-        <div className="top-right-navbar">loupe profil</div>
+    <div className="navbar">
+      <div className="logo_profil">
+      <Link to="/" className="logo">
+        <p >CINÉREVIEW</p>
+        </Link>
+        <div className="search">loupe </div>
+        <Link to="/profil" className="button-profil">
+          <p>Profil</p>
+        </Link>
       </div>
-      <div className="bottom-navbar">
-        <div className="bottom-left-navbar">FILMS SERIES GENRE</div>
-        <div className="watchlist">watchlist</div>
+
+
+      <div className="filter_content">
+        <div className="genre-film-series">
+      <Link to="/profil" className="button-filter">
+          <p>FILMS</p>
+        </Link>
+        <Link to="/series" className="button-filter">
+          <p>SERIES</p>
+        </Link>
+        <Link to="/genre" className="button-filter">
+          <p>GENRE</p>
+        </Link>
+        </div>
+        <Link to="/watchlist" className="button-watchlist">
+          <p>WATCHLIST</p>
+        </Link>
       </div>
     </div>
   );
