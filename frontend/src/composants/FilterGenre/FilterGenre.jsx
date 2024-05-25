0386ade75e23a71e1b03a1545e8b5d8012d7e1a4
Asle Sayed <asle@Asles-MacBook-Pro.local>
2024-05-25 23:37:59 +0200

    dropdown fixed

--- frontend/src/composants/FilterGenre/FilterGenre.jsx
+++ frontend/src/composants/FilterGenre/FilterGenre.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import "./filterGenre.css";
 
 function FilterGenre() {
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
 
-  const genres = ["HORROR", "ACTION", "COMEDY", "DRAMA", "THRILLER"];
+  const genres = ["Horror", "Action", "Comedy", "Drama", "Thriller"];
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className="filter-genre">
+    <div className="filter-genre" ref={ref}>
       <p className="button-filter" onClick={toggleDropdown}>
         GENRE {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
       </p>
