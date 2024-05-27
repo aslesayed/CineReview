741d1542435e467e5c3082007e2ec039d3667da9
Tristan AMATO <tristan.amato@edu.esce.fr>
2024-05-28 00:41:55 +0200

    footer done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./app.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./composants/Navbar/Navbar";
+import Footer from "./composants/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
