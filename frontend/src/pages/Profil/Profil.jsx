0616bbd6b0f124eb4ec29e4274ac8b9b989defa3
tristanamato <144697910+tristanamato@users.noreply.github.com>
2024-05-29 16:52:59 +0200

    Revert "Revert 7 connection"

---
+++ frontend/src/pages/Profil/Profil.jsx
@@ -0,0 +1,7 @@
+import "./profil.css";
+
+function Profil() {
+  return <div>Profil</div>;
+}
+
+export default Profil;
---
+++ frontend/src/pages/WatchList/WatchList.jsx
@@ -0,0 +1,7 @@
+import "./watchlist.css";
+
+function WatchList() {
+  return <div>hdaieh</div>;
+}
+
+export default WatchList;
