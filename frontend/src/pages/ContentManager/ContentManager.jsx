0d953ed46fb200a22236b8fa00e7b790460337c0
Asle Sayed <asle@Asles-MacBook-Pro.local>
2024-06-10 01:05:47 +0200

    css

--- frontend/src/pages/ContentManager/ContentManager.jsx
+++ frontend/src/pages/ContentManager/ContentManager.jsx
@@ -1,16 +1,35 @@
+// import "./contentmanager.css";
+// import ContentTable from "../../composants/ContentTable/ContentTable";
+// import ContentUpload from "../../composants/ContentUpload/ContentUpload";
+
+// function ContentManager() {
+//   return (
+//     <div className="content-manger" >
+
+//       <ContentUpload />
+//       <ContentTable />
+      
+//     </div>
+//   );
+// }
+
+// export default ContentManager;
 import "./contentmanager.css";
 import ContentTable from "../../composants/ContentTable/ContentTable";
 import ContentUpload from "../../composants/ContentUpload/ContentUpload";
+import Footer from "../../composants/Footer/Footer";
 
 function ContentManager() {
   return (
-    <div >
-
-      <ContentUpload />
-      <ContentTable />
-      
+    <div className="main-container">
+      <div className="content-wrapper">
+        <ContentUpload />
+        <ContentTable />
+      </div>
+      <Footer />
     </div>
   );
 }
 
 export default ContentManager;
+
