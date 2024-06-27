92d663457a57f96c2331c9e478d0287df17398f4
Asle Sayed <asle@Asles-MacBook-Pro.local>
2024-06-27 23:12:51 +0200

    content banckend

--- frontend/src/composants/ContentCard/ContentCard.jsx
+++ frontend/src/composants/ContentCard/ContentCard.jsx
@@ -1,36 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./contentcard.css";
 
-const fakeData = [
-  {
-    id: 1,
-    image: "https://via.placeholder.com/200x300.png?text=Movie+1",
-    genre: "Horror",
-    rating: "8.3",
-  },
-  {
-    id: 2,
-    image: "https://via.placeholder.com/200x300.png?text=Movie+2",
-    genre: "Drama",
-    rating: "7.5",
-  },
-  {
-    id: 3,
-    image: "https://via.placeholder.com/200x300.png?text=Movie+3",
-    genre: "Action",
-    rating: "9.1",
-  },
-  {
-    id: 4,
-    image: "https://via.placeholder.com/200x300.png?text=Movie+4",
-    genre: "Romance",
-    rating: "6.9",
-  },
-];
-
 const ContentCard = () => {
+  const [contents, setContents] = useState([]);
   const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const fetchContents = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/contents`
+        );
+        const data = await response.json();
+
+        setContents(data);
+      } catch (error) {
+        console.error("Error fetching contents:", error);
+      }
+    };
+
+    fetchContents();
+  }, []);
 
   const handleAddToFavorites = (contentId) => {
     setFavorites([...favorites, contentId]);
@@ -38,11 +29,15 @@ const ContentCard = () => {
 
   return (
     <div className="content-list">
-      {fakeData.map((content) => (
-        <Link to="/moviedetail" key={content.id} className="content-card-link">
+      {contents.map((content) => (
+        <Link
+          to={`/contents/${content.id}`}
+          key={content.id}
+          className="content-card-link"
+        >
           <div className="content-card">
             <img
-              src={content.image}
+              src={content.thumbnail}
               alt="Content Poster"
               className="content-image"
             />
